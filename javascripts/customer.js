09bb6d3764ee85e25ad9bdcf5ceb56bf635eb064
Wubai Zhou <zhouwubai@gmail.com>
2015-05-15 15:21:04 -0400

    testing recent_posts loader

--- javascripts/customer.js
+++ javascripts/customer.js
@@ -1,17 +1,16 @@
+var posts;
 function get_recent_posts(topk) {
     $.getJSON("/posts.json", function(data) {
-        var posts = data.posts.slice(0,topk);
+        posts = data.posts.slice(0,topk);
         var recent_post_tag = $("ul#recent_posts");
         $.each(posts, function(index, value) {
             //ref="/blog/2015/05/15/lda-implmentation-roadmap-distribution/
             var href = "/blog/"+value.year+"/"+value.month+"/"+value.day+"/"+value.title+"/"
             var title_text = value.title.trim().split("-").join(" ");
             recent_post_tag.append(
-            	$('<li>').apend(
+            	$('<li>').append(
 					$('<a>').attr('href',href).append(title_text)
 				));
         });//end each
     });//end getJson
 }//end function
-
-console.log("hello world");
